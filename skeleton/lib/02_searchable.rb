a71bff6006948755ebbabe8fa9976e5e7eac939c
Peter Poole <peter.j.poole@gmail.com>
2014-12-02 15:44:48 -0500

    Phase 2.

--- skeleton/lib/02_searchable.rb
+++ skeleton/lib/02_searchable.rb
@@ -3,10 +3,24 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map { |key| "#{key} = ?" }.join(" AND ")
+    values = params.values
+
+    results = DBConnection.execute(<<-SQL, *values)
+      SELECT
+        *
+      FROM
+        #{eval(self.name).table_name}
+      WHERE
+        #{where_line}
+    SQL
+
+    return [] if results.nil?
+
+    eval(self.name).parse_all(results)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
