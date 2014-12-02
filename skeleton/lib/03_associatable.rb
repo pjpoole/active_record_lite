a874438ee8dff6bf1dedf51649b862a8e8f16cc0
Peter Poole <peter.j.poole@gmail.com>
2014-12-02 16:02:01 -0500

    Options initializers.

--- skeleton/lib/03_associatable.rb
+++ skeleton/lib/03_associatable.rb
@@ -20,13 +20,19 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] ||
+      name.to_s.underscore.concat("_id").to_sym
+    @primary_key = options[:primary_key] || :id
+    @class_name  = options[:class_name]  || name.to_s.singularize.camelcase
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] ||
+      self_class_name.to_s.underscore.concat("_id").to_sym
+    @primary_key = options[:primary_key] || :id
+    @class_name  = options[:class_name]  || name.to_s.singularize.camelcase
   end
 end
 
